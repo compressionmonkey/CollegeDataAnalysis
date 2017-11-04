75c9646bb6147adbf8274c372d342b06e05c5a0c
Kelden <keldenddorji1997@gmail.com>
2017-11-04 21:51:40 +0600

    Fixed some of the bugs related to copying of cells

--- OpenpyXL.py
+++ OpenpyXL.py
@@ -1,27 +1,31 @@
 # Read in excel data
-from openpyxl import Workbook
+from openpyxl import workbook
 from openpyxl import load_workbook
 wb = load_workbook('Hiace2015.xlsx') #explicitly loading workbook, will automate later
-
+sheet = list(wb.active) # activating is an attribute and not a method
 sheetnames = list(wb.get_sheet_names())
 print sheetnames
-N = len(wb.rows)
-d = wb.cell(row=4, column=2, value=10)
-print d
-cell_range = ws['C7':'M7']
-col_range = wb['C:M']
-row_range = wb['9:31']
-def get_all_data(sheet_in):
-      Final_copy = Workbook()
-      Final_copy = Final_copy.active()
-      Final_copy.title = "Hiace Details"
-      Final_copy = Final_copy["Hiace Details"]
-      for row in row_range:
-          for column in col_range:
-            print row, column
 
-      wb.copy_worksheet()
-      wb.save('Final_Copy_Hiace_2016.xlsx')
+cells = sheet['C11':'M31']
+for c1, c2,c3,c4,c5,c6,c7,c8,c9,c10,c11 in cells:
+    print "{0:8} {1:8} {2:8} {3:8} {4:8} {5:8} {6:8} {7:8} {8:8} {9:8} {10:8}".format(c1.value, c2.value, c3.value, c4.value, c5.value, c6.value, c7.value, c8.value,
+                               c9.value, c10.value, c11.value)
+
+for row in sheet.iter_rows(min_row=20, min_col=11):
+    for cell in row:
+        cells.append(cell.value)
+    print("Number of values: {0}".format(len(values)))
+# def get_all_data(sheet_in):
+#       Final_copy = Workbook()
+#       Final_copy = Final_copy.active()
+#       Final_copy.title = "Hiace Details"
+#       Final_copy = Final_copy["Hiace Details"]
+#       for row in row_range:
+#           for column in col_range:
+#             print row, column
+#
+#       wb.copy_worksheet()
+#       wb.save('Final_Copy_Hiace_2016.xlsx')
 
 
             # create csv file that has all details to report on
