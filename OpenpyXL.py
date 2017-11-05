94e6e358d3ccc65bd4422b2a8490db3c63a9c424
Kelden <keldenddorji1997@gmail.com>
2017-11-05 13:53:49 +0600

    Tried to make changes to code to convert data types acceptable for model

--- OpenpyXL.py
+++ OpenpyXL.py
@@ -6,12 +6,13 @@ sheet = list(wb.active) # activating is an attribute and not a method
 sheetnames = list(wb.get_sheet_names())
 print sheetnames
 
-cells = sheet['C11':'M31']
+sheet = int(sheet)
+cells = sheet['C11:M31']
 for c1, c2,c3,c4,c5,c6,c7,c8,c9,c10,c11 in cells:
     print "{0:8} {1:8} {2:8} {3:8} {4:8} {5:8} {6:8} {7:8} {8:8} {9:8} {10:8}".format(c1.value, c2.value, c3.value, c4.value, c5.value, c6.value, c7.value, c8.value,
                                c9.value, c10.value, c11.value)
-
-for row in sheet.iter_rows(min_row=20, min_col=11):
+sum(sheet['C11': 'M31'].isnull())
+for row in sheet.iter_rows(cells):
     for cell in row:
         cells.append(cell.value)
     print("Number of values: {0}".format(len(values)))
