3eff4f7c39d6aa40e2fdf3a64b1152d978021ecc
Kelden <keldenddorji1997@gmail.com>
2017-11-29 17:40:25 +0600

    trying to build labelencorder

--- Predictive_Modeling_Example.py
+++ Predictive_Modeling_Example.py
@@ -25,7 +25,6 @@ cat_cols = ['StudentType','Gender','Program','HighSchool']
 # We will remove the ID_col, target_col, cat_cols from fullData
 num_cols= list(set(list(fullData.columns))-set(cat_cols)-set(ID_col)-set(target_col))
 other_col=['Type'] #Test and Train Data set identifier
-fullData[num_cols].mean(), inplace=True
 
 num_cat_cols = num_cols+cat_cols # Combined numerical and Categorical variables
 
@@ -37,11 +36,46 @@ for var in num_cat_cols:
         #Let's normalize data and assign it to a new variable
         fullData[var+'_NA'] = fullData[var].isnull()*1
 
-# Impute numerical missing values with mean
-fullData[num_cols] = fullData[num_cols].fillna(fullData[num_cols].mean(), inplace=True)
-# Impute categorical missing values with -9999
-fullData[cat_cols] = fullData[cat_cols].fillna(value=-9999)
+    # Impute numerical missing values with mean
+    fullData[num_cols] = fullData[num_cols].fillna(fullData[num_cols].mean(), inplace=True)
+    print fullData
+    # Impute categorical missing values with -9999
+    fullData[cat_cols] = fullData[cat_cols].fillna(value=-9999)
 
+    # create label encoders for categorical features
+    for var in cat_cols:
+        number = LabelEncoder()
+        # we will start labeling data
+        fullData[var] = number.fit_transform(fullData[var].astype('str'))
+        # tranforming centers the data i.e zero mean and unit standard error. x' = (x - standarderror)/ SD
+        # fit_transform finds x' while fit finds SE and SD but requires transform()
+    # Target variable is also a categorical so convert it
+    fullData["Class12PercentGrade"] = number.fit_transform(fullData["Class12PercentGrade"].astype('int32'))
+
+    train = fullData[fullData['Type'] == 'Train']
+    test = fullData[fullData['Type'] == 'Test']
+
+    train['is_train'] = np.random.uniform(0, 1, len(train)) <= .75
+    Train, Validate = train[train['is_train'] == True, train[train['is_train'] == False]
+
+    features = list(set(list(fullData.columns)) - set(ID_col) - set(target_col) - set(other_col))
+    x_train = Train[list(features)].values
+    y_train = Train["Class12PercentGrade"].values
+    x_validate = Validate[list(features)].values
+    y_validate = Validate["Class12PercentGrade"].values
+    x_test = test[list(features)].values
+    random.seed(100)
+    rf = RandomForestClassifier(n_estimators=1000)
+    rf.fit(x_train, y_train)
+
+    status = rf.predict_proba(x_validate)
+    fpr, tpr, _ = roc_curve(y_validate, status[:, 1])
+    roc_auc = auc(fpr, tpr)
+    print roc_auc
+
+    final_status = rf.predict_proba(x_test)
+    test["Class12PercentGrade"] = final_status[:, 1]
+    test.to_csv('model_output.csv', columns=[ID_col, target_col])
 # A predictive model can be boosted in two ways: Using feature engineering or boosting algorithms right away. Boosting
 #algorithms is faster than the other. Gradient Boosting, XGBoost, AdaBoost, GentleBoost etc are just some types of boosting
 # Algorithm. The answers can deviate abit as according.
